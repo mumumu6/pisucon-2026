5a13a1b39b46f004540ed84bbcde66b2697aa866
mumumu <takumu.sato@gmail.com>
2026-07-20 04:50:54 +0900

    doropabilityを0.1に

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1466,7 +1466,7 @@ func writeConditionBatch(batch []conditionWriteRequest) error {
 // ISUからのコンディションを受け取る
 func postIsuCondition(c echo.Context) error {
 	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-	dropProbability := 0.35
+	dropProbability := 0.1
 	if rand.Float64() <= dropProbability {
 		return c.NoContent(http.StatusAccepted)
 	}
