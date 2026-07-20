2eec31f42a059add5bab2477d1fca2036ead86f8
mumumu <takumu.sato@gmail.com>
2026-07-20 18:24:27 +0900

    ttl 上げる

--- webapp/go/models.go
+++ webapp/go/models.go
@@ -27,7 +27,7 @@ const (
 	scoreConditionLevelInfo     = 3
 	scoreConditionLevelWarning  = 2
 	scoreConditionLevelCritical = 1
-	trendCacheTTL               = 400 * time.Millisecond
+	trendCacheTTL               = 500 * time.Millisecond
 	trendCacheMaxAge            = 900 * time.Millisecond
 	conditionBatchMaxRequests   = 512
 	conditionBatchWait          = 1 * time.Millisecond
