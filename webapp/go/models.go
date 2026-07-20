d0a6cb32c50c4cc949e290848a52e56e527f80d6
mumumu <takumu.sato@gmail.com>
2026-07-20 21:00:08 +0900

    f

--- webapp/go/models.go
+++ webapp/go/models.go
@@ -31,8 +31,8 @@ const (
 	// trend TTL 配分（initialize 起点）
 	// 序盤は触らず、長いTTLの帯を早く始めて後半を厚くする。
 	// 0-5s / 5-10s / 10-20s / 20-35s / 35s-
-	trendTTL0                   = 1500 * time.Millisecond
-	trendMaxAge0                = 1800 * time.Millisecond
+	trendTTL0                   = 2000 * time.Millisecond
+	trendMaxAge0                = 2500 * time.Millisecond
 	trendTTL1                   = 2200 * time.Millisecond
 	trendMaxAge1                = 2500 * time.Millisecond
 	trendTTL2                   = 3500 * time.Millisecond
