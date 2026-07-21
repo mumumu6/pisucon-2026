9d4bd422e358d656e993590f41d6e620d7765466
mumumu <takumu.sato@gmail.com>
2026-07-21 17:12:41 +0900

    ip

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 # サーバー初期配布の env.sh（systemd の EnvironmentFile が読む）。
 # Ansible は触らない。DB を別ホストにするときだけ MYSQL_HOST を手で直す。
-MYSQL_HOST="127.0.0.1"
+MYSQL_HOST="10.0.0.121"
 MYSQL_PORT=3306
 MYSQL_USER=isucon
 MYSQL_DBNAME=isucondition
